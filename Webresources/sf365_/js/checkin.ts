7708e9a002b4c36ee6af7a2b88d44215ab6b7fcf
ychehih <yahia.chehih@gmail.com>
2020-04-19 13:19:21 +0200

    initial commit

---
+++ Webresources/sf365_/js/checkin.ts
@@ -0,0 +1,23 @@
+﻿/// <reference path="../../node_modules/@types/knockout/index.d.ts" />
+namespace sf365.checkin
+{
+
+    class CheckInViewModel
+    {
+        isBusy: KnockoutObservable<boolean>;
+
+        constructor() {
+            this.isBusy = ko.observable(false);
+        }
+
+        public foo() {
+            alert("foo");
+        }
+
+
+
+
+    }
+
+
+}
